5ed7396eb5571de8bcbcecb94972ec49c8af69f6
Mark Sia Hansheng <mark.siah91@gmail.com>
2017-07-16 13:46:58 +0800

    Add static and media root

--- core/settings.py
+++ core/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = "/media/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'public/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
